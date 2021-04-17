d268ab0e1ad44007ff8264d8797adf2d46fd1653
bca <techinha221@naver.com>
2021-04-17 13:03:02 +0900

    chapter3. Http Request parameter -queryParameter

---
+++ src/main/java/com/example/request/RequestParamController.java
@@ -0,0 +1,24 @@
+package com.example.request;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RestController
+public class RequestParamController {
+
+    @RequestMapping("/request-param-v1")
+    public void requestParamV1(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String userName = request.getParameter("userName");
+        int age = Integer.parseInt(request.getParameter("age"));
+
+        response.getWriter().write("ok");
+
+
+
+    }
+
+}
