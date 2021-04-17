eb209ab143a7222598b5cfb81e21e317c9cc9918
bca <techinha221@naver.com>
2021-04-17 14:02:02 +0900

    chapter4. @RequetParam case 정리

--- src/main/java/com/example/request/RequestParamController.java
+++ src/main/java/com/example/request/RequestParamController.java
@@ -1,13 +1,16 @@
 package com.example.request;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Map;
 
-@RestController
+@Controller
 public class RequestParamController {
 
     @RequestMapping("/request-param-v1")
@@ -17,8 +20,58 @@ public class RequestParamController {
 
         response.getWriter().write("ok");
 
-
-
     }
 
+    @ResponseBody
+    @RequestMapping("/request-param-v2")
+    public String requestParamV2(
+            @RequestParam("userName") String memberName,
+            @RequestParam("age") int memberAge) {
+
+        return "okay";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v3")
+    public String requestParamV3(
+            @RequestParam String memberName,
+            @RequestParam int memberAge) {
+
+        return "okay call";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v4")
+    public String requestParamV4(
+             String memberName,
+             int memberAge) {
+
+        return "okay";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v5")
+    public String requestParamV5(
+             @RequestParam(required = true) String memberName,
+             @RequestParam(required = false) Integer memberAge) {
+
+        return "okay calling";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v6")
+    public String requestParamV6(
+             @RequestParam(required = true, defaultValue = "guest") String memberName,
+             @RequestParam(required = false, defaultValue = "-1") int memberAge) {
+
+        return "okay calling";
+    }
+
+    @ResponseBody
+    @RequestMapping("/request-param-v7")
+    public String requestParamV7(
+             @RequestParam Map<String, String> paraMap) {
+
+        return "okay calling";
+    }
 }
